1416c2ce3e8f46a08acf6607dba48a186d2196a7
drh <jmnabangi@gmail.com>
2022-09-20 22:39:22 +0300

    initial setup - github action fixes
    Signed-off-by: drh <jmnabangi@gmail.com>

---
+++ config/tendepay.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    /*
+    |------------------------------------------------------
+    | Set sandbox mode
+    | ------------------------------------------------------
+    | Specify whether this is a test app or production app
+    |
+    | Sandbox base url: TODO()
+    | Production base url: TODO()
+    */
+    'sandbox' => env('TENDEPAY_SANDBOX', false),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache credentials/keys
+    |--------------------------------------------------------------------------
+    |
+    | If you decide to cache credentials, they will be kept in your app cache
+    | configuration for some time. Reducing the need for many requests for
+    | generating credentials/encryption keys
+    |
+    */
+    'cache_credentials' => true,
+
+
+];
