4ed00b7b875908d5b0e0085d489a502cf5cdcdaf
drh <jmnabangi@gmail.com>
2022-09-25 00:01:04 +0300

    updates callback and migration
    Signed-off-by: drh <jmnabangi@gmail.com>

--- src/Http/Controller.php
+++ src/Http/Controller.php
@@ -15,7 +15,18 @@ class Controller extends \Illuminate\Routing\Controller
 //        tendePayLogInfo("ipn: ", [$request]);
 
         try {
-            TendePayCallback::create($request->all());
+            TendePayCallback::create([
+                'initiator_reference' => $request->initiatorReference,
+                'response_code'       => $request->responseCode,
+                'status'              => $request->status,
+                'status_description'  => $request->statusDescription,
+                'amount'              => $request->amount,
+                'account_reference'   => $request->account_reference,
+                'confirmation_code'   => $request->confirmationcode,
+                'msisdn'              => $request->msisdn,
+                'receiver_party_name' => $request->receiverpartyname,
+                'date'                => $request->date,
+            ]);
 
             // TODO: Fire event
         } catch (QueryException $e) {
